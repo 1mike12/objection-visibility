cd56998fc284ee71c32d1b0e78c3285b0f0f3c4e
Michael Qin <1mike12@gmail.com>
2024-05-21 20:19:41 -0400

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,27 +1,25 @@
-
 interface IObjectType {
-    [k: string]: {};
+  [k: string]: {};
 }
 
-function includes(haystack: string[], needle: string) {
-    return haystack.indexOf(needle) !== -1;
+export function pick(data: IObjectType, toPick: Set<string>) {
+  return pickBy(data, toPick, (values, v) => values.has(v));
 }
 
-export function pick(data: IObjectType, toPick: string[]) {
-    return pickBy(data, toPick, (values, v) => includes(values, v));
+export function omit(data: IObjectType, toOmit: Set<string>) {
+  return pickBy(data, toOmit, (values, v) => !values.has(v));
 }
 
-export function omit(data: IObjectType, toOmit: string[]) {
-    return pickBy(data, toOmit, (values, v) => !includes(values, v));
-}
-
-function pickBy(data: IObjectType, values: string[], predicate: (values: string[], v: string) => boolean) {
-    return Object.keys(data)
-    .reduce((c: IObjectType, v) => {
-        if (predicate(values, v)) {
-            c[v] = data[v];
-            return c;
-        }
-        return c;
-    }, {});
+function pickBy(
+  data: IObjectType,
+  values: Set<string>,
+  predicate: (values: Set<string>, v: string) => boolean
+) {
+  return Object.keys(data).reduce((c: IObjectType, v) => {
+    if (predicate(values, v)) {
+      c[v] = data[v];
+      return c;
+    }
+    return c;
+  }, {});
 }
